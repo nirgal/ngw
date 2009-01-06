c01901d9b85240c59663a1dfe24371a4d67ccaf2
nirgal <nirgal@f19639c0-9264-4126-8c79-3e5b26eb25a8>
2009-01-06 17:09:55 +0000

    Better gpg bridge using --with-column
    git-svn-id: svn://localhost/ngw@163 f19639c0-9264-4126-8c79-3e5b26eb25a8

--- core/gpg.py
+++ core/gpg.py
@@ -17,36 +17,38 @@ def parse_pgp_listkey(output):
     keyring = {}
     for line in output.split('\n'):
         #print repr(line)
-        if line.startswith('pub'):
-            line = line[6:]
-            algo_id, dt = line.split(" ", 1)
-            algo, id = algo_id.split("/", 1)
-            #print algo, id, dt
-            keyring[id] = { 'uids': [], 'algo': algo, 'date': dt }
-        elif line.startswith('uid'):
-            p1 = line.find('<')
-            p2 = line.find('>')
-            #print p1,p2, len(line)
-            if p1==-1 or p2==-1 or p2!=len(line)-1:
-                print "Error parsing uid line in gpg --list-keys"
-                continue
-            mail = line[p1+1:p2]
-            name = line[4:p1].strip()
-            #print "*", mail, "*", name, "*", id
-            keyring[id]['uids'].append((name, mail))
+        items = line.split(':')
+        #print items
+        if items[0] == 'pub':
+            id = items[4]
+            keyring[id] = { 'uids': [ items[9] ], 'length': items[2], 'algo': items[3], 'date': items[5] }
+        elif items[0] == 'uid':
+            items = line.split(':')
+            keyring[id]['uids'].append(items[9])
+
+            #p1 = line.find('<')
+            #p2 = line.find('>')
+            ##print p1,p2, len(line)
+            #if p1==-1 or p2==-1 or p2!=len(line)-1:
+            #    print "Error parsing uid line in gpg --list-keys"
+            #    continue
+            #mail = line[p1+1:p2]
+            #name = line[4:p1].strip()
+            ##print "*", mail, "*", name, "*", id
+            #keyring[id]['uids'].append((name, mail))
     return keyring
 
 def is_email_secure(mail_address):
-    if mail_address.startswith("-"):
-        return False # Possible hack attempt
-    ret, out, err = subprocess_run("gpg", "--homedir", GPG_HOME, "--list-keys", mail_address)
+    #if mail_address.startswith("-"):
+    #    return False # Possible hack attempt
+    ret, out, err = subprocess_run("gpg", "--homedir", GPG_HOME, "--list-keys", "--with-colons", "<"+mail_address+">")
     if ret:
         return False # gpg error
     keyring_1 = parse_pgp_listkey(out)
     return len(keyring_1)>0
     
 def loadkeyring():
-    ret, out, err = subprocess_run("gpg", "--homedir", GPG_HOME, "--list-keys")
+    ret, out, err = subprocess_run("gpg", "--homedir", GPG_HOME, "--list-keys", "--with-colons")
     if ret:
         print err
         print "gpg failed."
