c11dc8d3863e8554126862377ecb0e9f9b27e442
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-01-02 17:27:48 +0100

    Added documentation about how to enable extensions

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,2 +1,5 @@
+# Prevents automatic loading of sub modules
+# To enable an extension, do not change this file. Just add it to
+# INSTALLED_APPS in settings
+
 __all__ = [ ]
-#__all__ = [ 'phpbb', 'mailman' ]
