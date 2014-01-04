e4324f575fb4d71782e6f8a2408667188fcd0a5f
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-01-04 17:01:16 +0100

    Migrate .htaccess fixer sqlalchemy->django

--- fix_static_htaccess.py
+++ fix_static_htaccess.py
@@ -5,7 +5,7 @@ import sys, os
 sys.path += [ '/usr/lib/' ]
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ngw.settings'
 
-from ngw.core.alchemy_models import *
+from ngw.core.models import ContactGroup
 
-for g in Query(ContactGroup):
+for g in ContactGroup.objects.all():
     g.check_static_folder_created()
