d7501a3d4ed759c883c0ba22b209d12c4241ecf3
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-01-06 18:41:23 +0100

    Database initialisation script
    Move phpbb3 documentation out of main doc.
    Documentation about database init scripts.

---
+++ sql/initialdata.sql
@@ -0,0 +1,141 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: choice_group; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY choice_group (id, name, sort_by_key) FROM stdin;
+1	Pays	f
+9	Oui / Non	t
+41	Status du mot de passe	t
+\.
+
+SELECT pg_catalog.setval('choice_group_id_seq', 46, true);
+
+--
+-- Data for Name: choice; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY choice (choice_group_id, key, value) FROM stdin;
+1	fr	France
+1	uk	Royaume Uni
+1	ch	Suisse
+1	it	Italie
+9	1	Oui
+9	2	Non
+41	1	Generated
+41	3	User defined
+41	2	Generated and mailed
+\.
+
+
+--
+-- Data for Name: config; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY config (id, text) FROM stdin;
+groups header	
+columns	name,field_7,field_8,field_10,field_52
+banner	<big>NGW</big> Group Ware
+phpbb acl dictionary	
+query_page_length	100
+\.
+
+
+--
+-- Data for Name: contact; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY contact (id, name) FROM stdin;
+1	Default admin
+\.
+
+SELECT pg_catalog.setval('contact_id_seq', 1080, true);
+
+--
+-- Data for Name: contact_group; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY contact_group (id, name, description, field_group, date, budget_code, system, mailman_address, has_news, sticky) FROM stdin;
+1	Contacts	Ensemble des contacts	t	\N		t		f	f
+2	Utilisateurs	Ensemble des personnes qui ont un identifiant et un mot de passe.\r\nVoir aussi "Utilisateurs NGW" et "Utilisateurs Forum".	t	\N		t	\N	f	f
+8	Admins	Ils peuvent ajouter des contacts dans n'importe quel groupe, et tout voir.	f	\N		t		f	f
+52	Utilisateurs NGW	Les personnes de ce groupe peuvent se connecter à la base de données.	f	\N		t	\N	f	f
+53	Utilisateurs Forum	Les personnes de ce groupe peuvent se connecter au forum (non disponible).	t	\N		t	\N	f	f
+\.
+
+SELECT pg_catalog.setval('contact_group_id_seq', 100, true);
+
+--
+-- Data for Name: contact_field; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY contact_field (id, name, hint, type, contact_group_id, sort_weight, choice_group_id, system, "default") FROM stdin;
+1	Login	Nom avec lequel vous vous connectez au système	TEXT	2	390	\N	t	\N
+2	Mot de passe	Ne pas modifier. Utiliser le bouton "Change password" après avoir cliqué sur votre nom en haut à droite.	PASSWORD	2	400	\N	t	\N
+3	Dernière connexion	Ce champ est mis à jour automatiquement	DATETIME	2	440	\N	t	\N
+4	Colonnes	Ce champ est mis à jour automatiquement	TEXT	2	450	\N	t	\N
+5	Filtres personnels	Ce champ est mis à jour automatiquement	TEXT	2	460	\N	t	\N
+7	Courriel		EMAIL	1	10	\N	f	\N
+8	GSM		PHONE	1	50	\N	f	\N
+9	Rue		LONGTEXT	1	110	\N	t	\N
+10	Tél.fixe		PHONE	1	30	\N	f	\N
+11	code postal		TEXT	1	120	\N	t	\N
+14	Ville		TEXT	1	130	\N	t	\N
+48	Pays		CHOICE	1	140	1	t	fr
+73	phpbb user id	Identifiant du forum. Ne pas toucher.	NUMBER	53	430	\N	t	\N
+74	Mot de passe nouveau		PASSWORD	2	410	\N	t	\N
+75	Status du mot de passe	Mis à jour automatiquement	CHOICE	2	420	41	t	\N
+\.
+
+SELECT pg_catalog.setval('contact_field_id_seq', 100, true);
+
+--
+-- Data for Name: contact_field_value; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY contact_field_value (contact_id, contact_field_id, value) FROM stdin;
+1	1	admin
+1	2	/BHLc/u80sjCs
+1	3	2013-10-23 10:19:15
+1	4	name,field_7,field_92,field_100
+1	7	test@exemple.net
+1	11	75000
+1	14	Paris
+1	48	fr
+1	74	hahlahT8
+1	75	3
+\.
+
+
+--
+-- Data for Name: contact_in_group; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY contact_in_group (contact_id, group_id, operator, member, invited, declined_invitation, note) FROM stdin;
+1	52	t	t	f	f	\N
+1	8	f	t	f	f	\N
+\.
+
+
+--
+-- Data for Name: group_in_group; Type: TABLE DATA; Schema: public; Owner: ngw
+--
+
+COPY group_in_group (father_id, subgroup_id) FROM stdin;
+1	2
+2	53
+2	52
+52	8
+\.
---
+++ sql/structure.sql
@@ -0,0 +1,544 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = true;
+
+--
+-- Name: contact_field_value; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_field_value (
+    contact_id integer NOT NULL,
+    contact_field_id integer NOT NULL,
+    value text
+);
+
+
+SET default_with_oids = false;
+
+--
+-- Name: contact_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE contact_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: contact; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact (
+    id integer DEFAULT nextval('contact_id_seq'::regclass) NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+SET default_with_oids = true;
+
+--
+-- Name: contact_in_group; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_in_group (
+    contact_id integer NOT NULL,
+    group_id integer NOT NULL,
+    operator boolean DEFAULT false NOT NULL,
+    member boolean DEFAULT false NOT NULL,
+    invited boolean DEFAULT false NOT NULL,
+    declined_invitation boolean DEFAULT false NOT NULL,
+    note text
+);
+
+
+--
+-- Name: contact_group_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE contact_group_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+SET default_with_oids = false;
+
+--
+-- Name: contact_group; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_group (
+    id integer DEFAULT nextval('contact_group_id_seq'::regclass) NOT NULL,
+    name character varying(255) NOT NULL,
+    description text,
+    field_group boolean DEFAULT false NOT NULL,
+    date date,
+    budget_code character varying(10) DEFAULT ''::character varying NOT NULL,
+    system boolean DEFAULT false NOT NULL,
+    mailman_address character varying(255),
+    has_news boolean DEFAULT false NOT NULL,
+    sticky boolean DEFAULT false NOT NULL
+);
+
+
+SET default_with_oids = true;
+
+--
+-- Name: choice; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE choice (
+    choice_group_id integer NOT NULL,
+    key character varying(255) DEFAULT ''::character varying NOT NULL,
+    value character varying(255) DEFAULT ''::character varying NOT NULL
+);
+
+
+--
+-- Name: choice_group_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE choice_group_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+SET default_with_oids = false;
+
+--
+-- Name: choice_group; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE choice_group (
+    id integer DEFAULT nextval('choice_group_id_seq'::regclass) NOT NULL,
+    name character varying(255),
+    sort_by_key boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- Name: config; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE config (
+    id character varying(32) NOT NULL,
+    text text
+);
+
+
+--
+-- Name: contact_field_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE contact_field_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: contact_field; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_field (
+    id integer DEFAULT nextval('contact_field_id_seq'::regclass) NOT NULL,
+    name character varying(255) NOT NULL,
+    hint text,
+    type character varying(15) DEFAULT 'TEXT'::character varying NOT NULL,
+    contact_group_id integer NOT NULL,
+    sort_weight integer NOT NULL,
+    choice_group_id integer,
+    system boolean DEFAULT false NOT NULL,
+    "default" text
+);
+
+
+--
+-- Name: contact_group_news; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_group_news (
+    id integer NOT NULL,
+    author_id integer,
+    contact_group_id integer,
+    date timestamp without time zone NOT NULL,
+    title text NOT NULL,
+    text text NOT NULL
+);
+
+
+--
+-- Name: contact_group_news_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE contact_group_news_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: contact_group_news_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE contact_group_news_id_seq OWNED BY contact_group_news.id;
+
+
+--
+-- Name: contact_sysmsg_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE contact_sysmsg_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: contact_sysmsg; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE contact_sysmsg (
+    id integer DEFAULT nextval('contact_sysmsg_id_seq'::regclass) NOT NULL,
+    contact_id integer NOT NULL,
+    message text NOT NULL
+);
+
+
+SET default_with_oids = true;
+
+--
+-- Name: group_in_group; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE group_in_group (
+    father_id integer NOT NULL,
+    subgroup_id integer NOT NULL
+);
+
+
+--
+-- Name: COLUMN group_in_group.father_id; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN group_in_group.father_id IS 'Automatic member';
+
+
+SET default_with_oids = false;
+
+--
+-- Name: log; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE log (
+    id integer NOT NULL,
+    dt timestamp without time zone NOT NULL,
+    contact_id integer NOT NULL,
+    action integer NOT NULL,
+    target text NOT NULL,
+    target_repr text NOT NULL,
+    property text,
+    property_repr text,
+    change text
+);
+
+
+--
+-- Name: log_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE log_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: log_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE log_id_seq OWNED BY log.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_group_news ALTER COLUMN id SET DEFAULT nextval('contact_group_news_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY log ALTER COLUMN id SET DEFAULT nextval('log_id_seq'::regclass);
+
+
+--
+-- Name: choice_group_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY choice_group
+    ADD CONSTRAINT choice_group_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: choice_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY choice
+    ADD CONSTRAINT choice_pkey PRIMARY KEY (choice_group_id, key);
+
+
+--
+-- Name: config_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY config
+    ADD CONSTRAINT config_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: contact_field_value_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_field_value
+    ADD CONSTRAINT contact_field_value_pkey PRIMARY KEY (contact_id, contact_field_id);
+
+
+--
+-- Name: contact_group_news_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_group_news
+    ADD CONSTRAINT contact_group_news_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: contact_in_group_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_in_group
+    ADD CONSTRAINT contact_in_group_pkey PRIMARY KEY (contact_id, group_id);
+
+
+--
+-- Name: contact_name_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact
+    ADD CONSTRAINT contact_name_key UNIQUE (name);
+
+
+--
+-- Name: contact_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact
+    ADD CONSTRAINT contact_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: contact_sysmsg_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_sysmsg
+    ADD CONSTRAINT contact_sysmsg_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: group_in_group_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY group_in_group
+    ADD CONSTRAINT group_in_group_pkey PRIMARY KEY (father_id, subgroup_id);
+
+
+--
+-- Name: group_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_group
+    ADD CONSTRAINT group_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: log_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY log
+    ADD CONSTRAINT log_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: xfield_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY contact_field
+    ADD CONSTRAINT xfield_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: choice_choice_group_id_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX choice_choice_group_id_index ON choice USING btree (choice_group_id);
+
+
+--
+-- Name: contact_field_value_contact_field_id_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX contact_field_value_contact_field_id_index ON contact_field_value USING btree (contact_field_id);
+
+
+--
+-- Name: contact_field_value_contact_id_index; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX contact_field_value_contact_id_index ON contact_field_value USING btree (contact_id);
+
+
+--
+-- Name: contact_sysmsg_contact_id_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX contact_sysmsg_contact_id_idx ON contact_sysmsg USING btree (contact_id);
+
+
+--
+-- Name: choice_choice_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY choice
+    ADD CONSTRAINT choice_choice_group_id_fkey FOREIGN KEY (choice_group_id) REFERENCES choice_group(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_field_choice_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_field
+    ADD CONSTRAINT contact_field_choice_group_id_fkey FOREIGN KEY (choice_group_id) REFERENCES choice_group(id) ON UPDATE CASCADE;
+
+
+--
+-- Name: contact_field_contact_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_field
+    ADD CONSTRAINT contact_field_contact_group_id_fkey FOREIGN KEY (contact_group_id) REFERENCES contact_group(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_field_value_contact_field_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_field_value
+    ADD CONSTRAINT contact_field_value_contact_field_id_fkey FOREIGN KEY (contact_field_id) REFERENCES contact_field(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_field_value_contact_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_field_value
+    ADD CONSTRAINT contact_field_value_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES contact(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_group_news_author_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_group_news
+    ADD CONSTRAINT contact_group_news_author_id_fkey FOREIGN KEY (author_id) REFERENCES contact(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_group_news_contact_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_group_news
+    ADD CONSTRAINT contact_group_news_contact_group_id_fkey FOREIGN KEY (contact_group_id) REFERENCES contact_group(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_in_group_contact_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_in_group
+    ADD CONSTRAINT contact_in_group_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES contact(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_in_group_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_in_group
+    ADD CONSTRAINT contact_in_group_group_id_fkey FOREIGN KEY (group_id) REFERENCES contact_group(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: contact_sysmsg_contact_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY contact_sysmsg
+    ADD CONSTRAINT contact_sysmsg_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES contact(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: group_in_group_child_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY group_in_group
+    ADD CONSTRAINT group_in_group_child_group_id_fkey FOREIGN KEY (subgroup_id) REFERENCES contact_group(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: group_in_group_father_group_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY group_in_group
+    ADD CONSTRAINT group_in_group_father_group_id_fkey FOREIGN KEY (father_id) REFERENCES contact_group(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: log_contact_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY log
+    ADD CONSTRAINT log_contact_id_fk FOREIGN KEY (contact_id) REFERENCES contact(id) MATCH FULL ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
