24928bd225b9964e6e3d3588fbf33fd84d2c1914
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-02-10 15:53:30 +0100

    Fixed logging format in xmmp extension
    Thanks pylint

--- extensions/xmpp/management/commands/ejabberd_auth.py
+++ extensions/xmpp/management/commands/ejabberd_auth.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-#
-# Database settings is defined in ~/.pgpass
 
 from __future__ import division, absolute_import, print_function, unicode_literals
 import sys
-import os
 import logging
 import struct
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
 from django.contrib.auth.hashers import check_password
-from ngw.core.models import ( ContactFieldValue, ContactGroup,
+from ngw.core.models import ( ContactFieldValue,
     FIELD_LOGIN, FIELD_PASSWORD )
 
 print('In ejabberd auth', file=sys.stderr)
 
 def send_result(result):
-    logging.debug('Sending result: %s' % result)
+    logging.debug('Sending result: %s', result)
     sys.stdout.write(struct.pack(b'>hh', 2, result and 1 or 0))
     sys.stdout.flush()
     logging.debug('Result sent')
@@ -27,58 +24,58 @@ def cmd_auth(login, domain, password):
     try:
         login_value = ContactFieldValue.objects.get(contact_field_id=FIELD_LOGIN, value=login)
     except ContactFieldValue.DoesNotExist:
-        logging.info('No user with login %s' % login)
+        logging.info('No user with login %s', login)
         return False
 
     cid = login_value.contact_id
     contact = login_value.contact
     if not contact.is_member_of(settings.XMPP_GROUP):
-        logging.info('User %s is not member of group XMPP')
+        logging.info('User %s is not member of group XMPP', login)
         return False
 
     try:
         dbpassword = ContactFieldValue.objects.get(contact_id=cid, contact_field_id=FIELD_PASSWORD).value
     except ContactFieldValue.DoesNotExist:
-        logging.info('User %s has no password' % login)
+        logging.info('User %s has no password', login)
         return False
 
-    logging.debug('Checking passord for user %s' % login)
+    logging.debug('Checking passord for user %s', login)
     if check_password(password, 'crypt$$'+dbpassword):
-        logging.info('User %s auth successful' % login)
+        logging.info('User %s auth successful', login)
         return True
     else:
-        logging.info('Bad password for user %s' % login)
+        logging.info('Bad password for user %s', login)
         return False
 
 def cmd_isuser(login, domain):
     try:
         login_value = ContactFieldValue.objects.get(contact_field_id=FIELD_LOGIN, value=login)
     except ContactFieldValue.DoesNotExist:
-        logging.info('No user with login %s' % login)
+        logging.info('No user with login %s', login)
         return False
 
     cid = login_value.contact_id
     contact = login_value.contact
     if not contact.is_member_of(settings.XMPP_GROUP):
-        logging.info('User %s is not member of group XMPP')
+        logging.info('User %s is not member of group XMPP', login)
         return False
     return True
 
 def main():
     logging.debug('Incoming connection. Reading command length...')
     cmdlength = sys.stdin.read(2)
-    logging.debug('Received %s' % repr(cmdlength))
+    logging.debug('Received %s', repr(cmdlength))
     cmdlength, = struct.unpack(b'>h', cmdlength)
 
-    logging.debug('Reading %s bytes long command...' % cmdlength)
+    logging.debug('Reading %s bytes long command...', cmdlength)
     bindata = sys.stdin.read(cmdlength)
 
-    logging.debug('Received command: %s' % repr(bindata))
+    logging.debug('Received command: %s', repr(bindata))
     data = unicode(bindata, 'utf-8', 'strict')
 
     data = data.split(b':', 1)
     if len(data) != 2:
-        logging.error('Command %s has no arguments' % data)
+        logging.error('Command %s has no arguments', data)
     cmd, args = data
     args = args.split(':')
     if cmd == 'auth':
