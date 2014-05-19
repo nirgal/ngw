d4e13ab2a3b9f3e1bb8ed74839aba2f51d416e12
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-05-19 17:27:14 +0200

    Django style email backend supporting tor

--- core/sendmail.py
+++ core/sendmail.py
@@ -3,107 +3,20 @@
 
 from __future__ import division, absolute_import, print_function, unicode_literals
 
-import socket
-import smtplib
-import ssl
 import httplib
 import urllib
 import json
 import logging
-import socks # You need package python-socksipy
-from email.mime.text import MIMEText
 from django.conf import settings
+from django.core.mail import send_mass_mail
 
 
-def _validate_wildcard_name(valid_for, expected):
-    '''
-    Returns True if hostname valid_for that may start with wildcard matches
-    expected.
-    For exemple, expected='smtp.example.com' will match 'smtp.example.com',
-    '*.example.com', but not 'www.example.com'.
-    '''
-    #print('_validate_wildcard_name:', valid_for, expected)
-    if not valid_for:
-        return False
-    if valid_for[0] == '*':
-        wanted_tail = valid_for[1:]
-        return wanted_tail == expected[-len(wanted_tail):]
-    else:
-        return valid_for == expected
 
+def send_mail1(addresses, text):
+    subject = 'You have a message'
+    send_mass_mail([(subject, text, None, [recipient])
+        for recipient in addresses])
 
-def validate_ssl_hostname(cert, expected_sslhostname):
-    '''
-    Tests whether certificate provides expected_sslhostname
-    Returns a boolean
-    '''
-    for line in cert['subject']:
-        key, value = line[0]
-        if key == 'commonName':
-            if _validate_wildcard_name(value, expected_sslhostname):
-                return True
-    for line in cert.get('subjectAltName', ()):
-        key, value = line
-        if key == 'DNS':
-            if _validate_wildcard_name(value, expected_sslhostname):
-                return True
-    return False
-
-
-class SMTP_SSL_TOR(smtplib.SMTP_SSL):
-    '''
-    This is a smtplib.SMTP like object, but that use local TOR.
-    Only support smtps, sslv3. Does enforce certificate validation.
-    '''
-
-    def _get_socket(self, host, port, timeout):
-        
-        s = socks.socksocket()
-        s.setproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050, True)
-
-        s.connect((host, port))
-        if timeout is not socket._GLOBAL_DEFAULT_TIMEOUT:
-            s.settimeout(timeout)
-
-        new_socket = ssl.wrap_socket(s, self.keyfile, self.certfile,
-            ssl_version=ssl.PROTOCOL_SSLv3, # v2 is insecure
-            ca_certs='/etc/ssl/certs/ca-certificates.crt',
-            cert_reqs=ssl.CERT_REQUIRED)
-        cert = new_socket.getpeercert()
-        if not cert:
-            raise smtplib.SMTPException("Ssl certificate of remote smtp server was not validated by any known authority.")
-
-        # Allow the use of EMAIL_EXPECTED_SSLHOSTNAME to override default
-        # value of settings.EMAIL_HOST
-        try:
-            expected_sslhostname = settings.EMAIL_EXPECTED_SSLHOSTNAME
-        except AttributeError:
-            expected_sslhostname = settings.EMAIL_HOST
-        if not validate_ssl_hostname(cert, expected_sslhostname):
-            raise smtplib.SMTPException('Ssl certificate is valid but does not match %s.' % expected_sslhostname)
-
-        self.file = smtplib.SSLFakeFile(new_socket)
-        return new_socket
-
-
-def send_mail(addresses, message):
-    assert settings.EMAIL_PORT==465, 'Sorry we only support smtps connections right now'
-
-    charset = settings.DEFAULT_CHARSET
-    msg = MIMEText(message.encode(charset), _charset=charset)
-    msg['Subject'] = 'You have a message'
-    msg['From'] = settings.DEFAULT_FROM_EMAIL
-    msg['To'] = 'undisclosed-recipients:;'
-
-    if b'.onion' in settings.EMAIL_HOST:
-        s = SMTP_SSL_TOR(settings.EMAIL_HOST, settings.EMAIL_PORT, local_hostname='[::1]')
-    else:
-        s = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
-    s.set_debuglevel(1)
-    if settings.EMAIL_HOST_USER:
-        s.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-    s.sendmail(settings.DEFAULT_FROM_EMAIL, addresses, msg.as_string())
-    s.quit()
 
 def send_mail2(addresses, message):
     conn = httplib.HTTPSConnection('onetime.info')
@@ -111,7 +24,7 @@ def send_mail2(addresses, message):
         conn.request('POST', '/', urllib.urlencode({
             'message': message.encode(settings.DEFAULT_CHARSET),
             'once': True,
-            'expiration': '7',
+            'expiration': '1',
         }), {
             'Content-type': 'application/x-www-form-urlencoded',
             'X_REQUESTED_WITH': 'XMLHttpRequest',
@@ -123,7 +36,7 @@ def send_mail2(addresses, message):
             logging.error("%s", response.read())
         jresponse = json.load(response)
 
-        send_mail([address], '''Hello
+        send_mail1([address], '''Hello
 
 You can read your message at https://onetime.info%s
 
@@ -132,6 +45,6 @@ a pen ready before clicking the link. :)''' % jresponse['url'])
 
 
 if __name__ == '__main__':
-    send_mail2(['user@localhost'], '''Hello
+    send_mail2([settings.DEFAULT_FROM_EMAIL], '''Hello
 
 This is yet another test.''')
---
+++ core/tormailbackend.py
@@ -0,0 +1,159 @@
+# -*- encoding: utf-8 -*-
+'''
+This module provides TorEmailBackend, a backend to send mail through a SMTPS
+server available through TOR.
+'''
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+import os
+import time
+import random
+import socket
+import smtplib
+import ssl
+import socks # You need package python-socksipy
+from django.conf import settings
+from django.core.mail.backends.smtp import EmailBackend
+from django.core.mail import make_msgid
+
+def _validate_wildcard_name(valid_for, expected):
+    '''
+    Returns True if hostname valid_for that may start with wildcard matches
+    expected.
+    For exemple, expected='smtp.example.com' will match 'smtp.example.com',
+    '*.example.com', but not 'www.example.com'.
+    '''
+    #print('_validate_wildcard_name:', valid_for, expected)
+    if not valid_for:
+        return False
+    if valid_for[0] == '*':
+        wanted_tail = valid_for[1:]
+        return wanted_tail == expected[-len(wanted_tail):]
+    else:
+        return valid_for == expected
+
+
+def validate_ssl_hostname(cert, expected_sslhostname):
+    '''
+    Tests whether certificate provides expected_sslhostname
+    Returns a boolean
+    '''
+    for line in cert['subject']:
+        key, value = line[0]
+        if key == 'commonName':
+            if _validate_wildcard_name(value, expected_sslhostname):
+                return True
+    for line in cert.get('subjectAltName', ()):
+        key, value = line
+        if key == 'DNS':
+            if _validate_wildcard_name(value, expected_sslhostname):
+                return True
+    return False
+
+
+class SMTP_SSL_TOR(smtplib.SMTP_SSL):
+    '''
+    This is a smtplib.SMTP like object, but that use local TOR.
+    Only support smtps, sslv3. Does enforce certificate validation.
+    TODO: starttls would be better, but python2 checks are a pain. Waiting for
+    python3 support in debian django...
+    '''
+
+    def _get_socket(self, host, port, timeout):
+        
+        s = socks.socksocket()
+        s.setproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050, True)
+
+        s.connect((host, port))
+        if timeout is not socket._GLOBAL_DEFAULT_TIMEOUT:
+            s.settimeout(timeout)
+
+        new_socket = ssl.wrap_socket(s, self.keyfile, self.certfile,
+            ssl_version=ssl.PROTOCOL_SSLv3, # v2 is insecure
+            ca_certs='/etc/ssl/certs/ca-certificates.crt',
+            cert_reqs=ssl.CERT_REQUIRED)
+        cert = new_socket.getpeercert()
+        if not cert:
+            raise smtplib.SMTPException("Ssl certificate of remote smtp server was not validated by any known authority.")
+
+        # Allow the use of EMAIL_EXPECTED_SSLHOSTNAME to override default
+        # value of settings.EMAIL_HOST
+        try:
+            expected_sslhostname = settings.EMAIL_EXPECTED_SSLHOSTNAME
+        except AttributeError:
+            expected_sslhostname = settings.EMAIL_HOST
+        if not validate_ssl_hostname(cert, expected_sslhostname):
+            raise smtplib.SMTPException('Ssl certificate is valid but does not match %s.' % expected_sslhostname)
+
+        self.file = smtplib.SSLFakeFile(new_socket)
+        return new_socket
+
+
+
+def make_msgid_noFQDN(idstring=None):
+    '''
+    This is a clone of mail.make_msgid
+    But it does NOT use fully qualified domain name.
+    '''
+    timeval = time.time()
+    utcdate = time.strftime('%Y%m%d%H%M%S', time.gmtime(timeval))
+    try:
+        pid = os.getpid()
+    except AttributeError:
+        # No getpid() in Jython, for example.
+        pid = 1
+    randint = random.randrange(100000)
+    if idstring is None:
+        idstring = ''
+    else:
+        idstring = '.' + idstring
+    idhost = 'localhost.localnet'
+    msgid = '<%s.%s.%s%s@%s>' % (utcdate, pid, randint, idstring, idhost)
+    return msgid
+
+
+class TorEmailBackend(EmailBackend):
+    '''
+    This is a django-like email backend.
+    But it will open a tor-proxy connection if hostname contains '.onion'
+    '''
+
+    def open(self):
+        """
+        Ensures we have a connection to the email server. Returns whether or
+        not a new connection was required (True or False).
+        """
+        if self.connection:
+            # Nothing to do if the connection is already open.
+            return False
+        try:
+            assert self.port==465, 'Sorry we only support smtps connections right now'
+
+            if b'.onion' in self.host:
+                self.connection = SMTP_SSL_TOR(self.host, self.port,
+                    local_hostname='[::1]')
+            else:
+                # If local_hostname is not specified, socket.getfqdn() gets used.
+                # For performance, we use the cached FQDN for local_hostname.
+                self.connection = smtplib.SMTP_SSL(self.host, self.port,
+                                           local_hostname=DNS_NAME.get_fqdn())
+            self.connection.set_debuglevel(1)
+            if self.use_tls:
+                self.connection.ehlo()
+                self.connection.starttls()
+                self.connection.ehlo()
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+            return True
+        except:
+            if not self.fail_silently:
+                raise
+
+    def _send(self, email_message):
+        '''
+        This is the low level send function.
+        Patch the headers so that message-id does NOT contain the fully
+        qualified domain name.
+        '''
+        email_message.extra_headers['Message-ID'] = make_msgid_noFQDN()
+        super(TorEmailBackend, self)._send(email_message)
