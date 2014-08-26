1b221ce82e1d6a6244f5562551301a60fb5f6be3
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-08-26 15:31:49 +0200

    New tool to recover zombie contacts
    Some old database have contacts that are not in contact group.
    That tool list them.

---
+++ core/management/commands/recover_contacts.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+import logging
+from django.core.management.base import NoArgsCommand
+from ngw.core.models import Contact, GROUP_EVERYBODY, CIGFLAG_MEMBER
+
+class Command(NoArgsCommand):
+    help = 'Recover lost contacts'
+
+    def handle_noargs(self, **options):
+        logger = logging.getLogger('contactrecover')
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)-8s %(message)s'))
+        logger.addHandler(handler)
+        logger.propagate = False
+        verbosity = int(options['verbosity'])
+        if verbosity == 0:
+            logger.setLevel(logging.ERROR)
+        elif verbosity == 1:
+            logger.setLevel(logging.WARNING)
+        elif verbosity == 2:
+            logger.setLevel(logging.INFO)
+        elif verbosity == 3:
+            logger.setLevel(logging.DEBUG)
+
+        for contact in Contact.objects.extra(where=['NOT EXISTS (SELECT * FROM contact_in_group WHERE contact_id=contact.id AND group_id IN (SELECT self_and_subgroups(%s)) AND flags & %s <> 0)' % (GROUP_EVERYBODY, CIGFLAG_MEMBER)]):
+            logger.error('%s is not member of group EVERYBODY', contact)
