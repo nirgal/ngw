beea60a0dd0b102540f367e670a3afb89da99ac6
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-08-28 11:28:04 +0200

    Proper sort in group overview redirect

--- core/views/groups.py
+++ core/views/groups.py
@@ -257,7 +257,7 @@ def contactgroup_index(request, gid):
     if perms.c_can_see_files_cg(request.user.id, gid):
         return HttpResponseRedirect(cg.get_absolute_url() + 'files/')
     if perms.c_can_view_msgs_cg(request.user.id, gid):
-        return HttpResponseRedirect(cg.get_absolute_url() + 'messages/')
+        return HttpResponseRedirect(cg.get_absolute_url() + 'messages/?&_order=-0')
     raise PermissionDenied
 
 
