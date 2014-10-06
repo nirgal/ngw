75530fde0086de75d26a7f3042e2a7ff4ed0edf6
Jean-Michel Vourgère <jmv_deb@nirgal.com>
2014-10-06 16:55:40 +0200

    Typo in sql function

--- sql/functions.sql
+++ sql/functions.sql
@@ -423,15 +423,10 @@ CREATE OR REPLACE VIEW v_cig_perm(contact_id, group_id, flags) AS
 -- Can contact_id_1 see contact_id_2
 -- Meaning there is a group whose members can be seen by contact_id_1 and in which is contact_id_2 is member
 CREATE OR REPLACE VIEW v_c_can_see_c(contact_id_1, contact_id_2) AS
-    SELECT DISTINCT v_cig_perm.contact_id AS contact_id_1, v_c_member_of.contact_id
+    SELECT DISTINCT v_cig_perm.contact_id AS contact_id_1, v_c_appears_in_cg.contact_id
     FROM v_cig_perm
     JOIN v_c_appears_in_cg ON v_cig_perm.group_id=v_c_appears_in_cg.group_id
     WHERE flags & 128 <> 0
     ;
-
--- CREATE OR REPLACE FUNCTION perm_c_can_see_c(integer, integer) RETURNS boolean
--- LANGUAGE SQL STABLE AS $$
---      SELECT EXISTS(SELECT * FROM v_c_can_see_c WHERE contact_id_1=$1 AND contact_id_2=$2);
--- $$;
 
 -- vim: set et ts=4 ft=sql:
