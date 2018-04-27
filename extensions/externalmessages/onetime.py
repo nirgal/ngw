2b043cc7ffe8639b63c0f57916f06b3c275dd8e7
Jean-Michel Nirgal Vourgère <jmv_deb@nirgal.com>
2018-04-27 10:29:33 +0200

    Back to give up on RCPT TO reject

--- extensions/externalmessages/onetime.py
+++ extensions/externalmessages/onetime.py
@@ -195,17 +195,15 @@ one to read it, please repport that.</p>''')
         sync_info['email_sent'] = True
 
     except smtplib.SMTPRecipientsRefused as err:
-        logger.critical('SMTPRecipientsRefused: %s', err)
-        return
         # Here, err.recipients is a dictionary with a single entry, like
         # { 'toto@riseup.net': (550, b'5.1.1 <eliandre@riseup.net>:
         # Recipient address rejected: User unknown')}
-        # errmsg = str(err.recipients.popitem()[1][1], 'utf-8', 'replace')
-        # logger.warning(
-        #     'Giving up on onetime message notification for message %s: %s',
-        #     msg.id, errmsg)
-        # sync_info['email_sent'] = False
-        # # no return here
+        errmsg = str(err.recipients.popitem()[1][1], 'utf-8', 'replace')
+        logger.warning(
+            'Giving up on onetime message notification for message %s: %s',
+            msg.id, errmsg)
+        sync_info['email_sent'] = False
+        # no return here
 
     except smtplib.SMTPResponseException as err:
         logger.critical('SMTPResponseException: %s', err)
